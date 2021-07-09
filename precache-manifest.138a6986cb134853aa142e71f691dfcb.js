f367bd84de30011a797c753b4bbae4ba2d0a9464
Eduardo Baranowski <eduardobaranowski@gmail.com>
2021-07-08 23:04:06 -0300

    Updates

---
+++ precache-manifest.138a6986cb134853aa142e71f691dfcb.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "e95409336ef9c6edecd5",
+    "url": "/lojadetenenisreactnative/static/js/app.0e0c8d9c.chunk.js"
+  },
+  {
+    "revision": "6d483a381e04483242dd",
+    "url": "/lojadetenenisreactnative/static/js/runtime~app.b1138d9f.js"
+  },
+  {
+    "revision": "cecedb4dcea5831188fa",
+    "url": "/lojadetenenisreactnative/static/js/2.bc8fc6e0.chunk.js"
+  },
+  {
+    "revision": "643b0683fcaddcb2df208700dca81239",
+    "url": "/lojadetenenisreactnative/static/media/1.3fc500af.png"
+  },
+  {
+    "revision": "a59442bd4cc2795a27c4932169407ec5",
+    "url": "/lojadetenenisreactnative/static/media/3.cb1c2a6d.png"
+  },
+  {
+    "revision": "af0878f4783ae0f94aa656730cb1d65c",
+    "url": "/lojadetenenisreactnative/static/media/4.faf2f39f.png"
+  },
+  {
+    "revision": "a37b0c01c0baf1888ca812cc0508f6e2",
+    "url": "/lojadetenenisreactnative/./fonts/MaterialIcons.ttf"
+  },
+  {
+    "revision": "ca9ce9ff0676a9b04ef0f8a3ad17dd08",
+    "url": "/lojadetenenisreactnative/./fonts/Feather.ttf"
+  },
+  {
+    "revision": "10c6818802ed96f9d6976633ae148c37",
+    "url": "/lojadetenenisreactnative/./fonts/Anton_400Regular.ttf"
+  },
+  {
+    "revision": "86b7baafebc10cadb52191684a49b3d5",
+    "url": "/lojadetenenisreactnative/static/media/banner.30fe929b.png"
+  },
+  {
+    "revision": "1c882f3529f09532b061119d1eea1937",
+    "url": "/lojadetenenisreactnative/static/media/2.47f70016.png"
+  },
+  {
+    "revision": "06d86e6cd1110213286afcca8809b82e",
+    "url": "/lojadetenenisreactnative/static/media/detail.c5ba8a70.png"
+  },
+  {
+    "revision": "3a2ba31570920eeb9b1d217cabe58315",
+    "url": "/lojadetenenisreactnative/./fonts/AntDesign.ttf"
+  },
+  {
+    "revision": "744ce60078c17d86006dd0edabcd59a7",
+    "url": "/lojadetenenisreactnative/./fonts/Entypo.ttf"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/lojadetenenisreactnative/./fonts/FontAwesome.ttf"
+  },
+  {
+    "revision": "c39278f7abfc798a241551194f55e29f",
+    "url": "/lojadetenenisreactnative/./fonts/FontAwesome5_Brands.ttf"
+  },
+  {
+    "revision": "b70cea0339374107969eb53e5b1f603f",
+    "url": "/lojadetenenisreactnative/./fonts/FontAwesome5_Solid.ttf"
+  },
+  {
+    "revision": "b49ae8ab2dbccb02c4d11caaacf09eab",
+    "url": "/lojadetenenisreactnative/./fonts/Fontisto.ttf"
+  },
+  {
+    "revision": "e20945d7c929279ef7a6f1db184a4470",
+    "url": "/lojadetenenisreactnative/./fonts/Foundation.ttf"
+  },
+  {
+    "revision": "b2e0fc821c6886fb3940f85a3320003e",
+    "url": "/lojadetenenisreactnative/./fonts/Ionicons.ttf"
+  },
+  {
+    "revision": "3c851d60ad5ef3f2fe43ebd263490d78",
+    "url": "/lojadetenenisreactnative/./fonts/MaterialCommunityIcons.ttf"
+  },
+  {
+    "revision": "d2285965fe34b05465047401b8595dd0",
+    "url": "/lojadetenenisreactnative/./fonts/SimpleLineIcons.ttf"
+  },
+  {
+    "revision": "b04737df41ba4566848921cd0bdb15fb",
+    "url": "/lojadetenenisreactnative/index.html"
+  },
+  {
+    "revision": "ec543248d7b23864564429fc03837190",
+    "url": "/lojadetenenisreactnative/serve.json"
+  },
+  {
+    "revision": "2002987acac9deb6803ab4ba381226ba",
+    "url": "/lojadetenenisreactnative/expo-service-worker.js"
+  },
+  {
+    "revision": "ddac6d82e8e7a095b69457cd6fc9bf5d",
+    "url": "/lojadetenenisreactnative/register-service-worker.js"
+  },
+  {
+    "revision": "1cca25f2e61cdb7dd28b898e81578b23",
+    "url": "/lojadetenenisreactnative/static/js/2.bc8fc6e0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d324ddc823eca6c86688f62576c81e76",
+    "url": "/lojadetenenisreactnative/favicon-16.png"
+  },
+  {
+    "revision": "e72daee58e37c99d3553059eda418aa8",
+    "url": "/lojadetenenisreactnative/favicon-32.png"
+  },
+  {
+    "revision": "50b1bd72d8b0c569ffcad83d622ec176",
+    "url": "/lojadetenenisreactnative/favicon.ico"
+  },
+  {
+    "revision": "74086983ff38d4597b2e2a64cdc90469",
+    "url": "/lojadetenenisreactnative/manifest.json"
+  }
+];
